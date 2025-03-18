bb7e46804182182b5a6d392b97915914588055d9
Everton C Prado Jr <ecpj.prado@gmail.com>
2025-03-18 09:49:56 -0300

    Structs

---
+++ rectangles/src/main.rs
@@ -0,0 +1,60 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.height > other.height && self.width > other.width
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn set_width(&mut self, width: u32) {
+        self.width = width;
+    }
+
+    fn max(self, other: Rectangle) -> Rectangle {
+        Rectangle {
+            width: self.width.max(other.width),
+            height: self.height.max(other.height),
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 60,
+    };
+
+    let mut rect = Rectangle::square(0);
+    let other_rect = Rectangle::square(1);
+
+    rect.set_width(20); //                          Write permission
+    println!("Square area: {}", rect.area()); //    Read  permission
+    let max_rect = rect.max(other_rect); //         Owner permission
+
+    dbg!(max_rect);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+}
